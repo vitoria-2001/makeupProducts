43bc0ac6b49744a1770a8a6c7fb582d56e759815
Vitória Amorim <vitoria.amorimm01@gmail.com>
2021-04-23 15:04:34 -0400

    add filter functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
-import React from  'react';
+import React, { useEffect, useState } from  'react';
 
-export default function App() {
-
-async function doFetchProducts() {
+const doFetchProducts= async () => {
   const resource= await fetch("https://makeup-api.herokuapp.com/api/v1/products.json");
   const json = await resource.json();
 
-  console.log(json)
+  return json.map((product) => {
+    return {
+      id: product.id,
+      name: product.name,
+      product_type: product.product_type,
+      category: product.category,
+      brand: product.brand,
+      price:product.price,
+      price_sign: product.price_sign,
+      img: product.image_link
+    };
+  });
 }
+
+export default function App() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [requiredProduct, setRequiredProduct] = useState('');
+  const [foundProducts, setFoundProducts] = useState([]);
+  const [totalFoundProducts, setTotalFoundProducts] = useState(0);
+
+  useEffect(() => {
+    const getProducts = async () => {
+        allProducts = await doFetchProducts();
+      setAllProducts(allProducts);
+      setFoundProducts(Object.assign([], allProducts));
+    };
+
+    getProducts();
+  }, []);
+
+  //Para filtrar os produtos a partir do texto digitado
+  
+  useEffect (() => {
+    const filterTextProduct = requiredProduct.toLowerCase();
+
+     foundProducts = allProducts.filter((product) => {
+      return product.nameFiltered.includes(filterTextProduct);
+    });
+
+    setFoundProducts(foundProducts);
+    setTotalFoundProducts(foundProducts.length);
+  }, [allProducts, requiredProduct]);
 
   return (
     <div>
-      <h3>OIII </h3>
+      <nav>MAKEUP | PRODUCTS</nav>
+
+    <div>
+      <input
+      placeholder="Seach a product" 
+      type="text"
+      value={requiredProduct}
+      onChange={handleRequiredProduct}
+      />
+    </div>
+
+      
     </div>
   );
 }
